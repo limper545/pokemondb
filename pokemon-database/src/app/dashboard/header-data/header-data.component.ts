d4a0a0afad8bd49c709d949f7c8d4f5dadab9452
limper545 <limper545@live.com>
2019-06-08 21:58:19 +0200

    Added bootstrap grid

--- pokemon-database/src/app/dashboard/header-data/header-data.component.ts
+++ pokemon-database/src/app/dashboard/header-data/header-data.component.ts
@@ -9,10 +9,11 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HeaderDataComponent implements OnInit {
 
+  // FIXME: Keine Bilder aus dem Internet laden
   imageSources = [
-    'https://cloud.elbenwald.de/blog/Pokemon/Pokemon-Titel.png',
-    'https://cloud.elbenwald.de/blog/Pokemon/Bild%202%20Kanto.jpg',
-    'https://cloud.elbenwald.de/blog/Pokemon/Bild%209%20SoulSilver%20Lugia.jpg'
+    'https://www.lunchactually.com/ch/wp-content/uploads/bfi_thumb/Pokemon-pokemon-13598061-1280-1024-n2boa7sxzkgdc09dp655n80m1wphbv42dt9pnita60.jpg',
+    'https://thehivegaming.s3.us-west-2.amazonaws.com/uploads/2019/01/mCuiXT4KER5lUUbJlCeDS4WcSwgcrKCvHlmRc3vC.jpeg',
+    'https://2ij7hk3p1vra3uhoox411gzf-wpengine.netdna-ssl.com/wp-content/uploads/bfi_thumb/Pokemon-Go-1-o6az9t59e7qemgfdlyztkp1sr63556mo888fqplmm0.jpg'
   ];
 
   constructor(config: NgbCarouselConfig) {
