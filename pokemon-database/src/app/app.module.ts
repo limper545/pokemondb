a92d799d940690f1f00af7c0680e8214259c0b05
limper545 <limper545@live.com>
2019-06-11 00:59:10 +0200

    added ngrx to app

--- pokemon-database/src/app/app.module.ts
+++ pokemon-database/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { PokemonDetailsComponent } from './pokemon-db/pokemon-details/pokemon-details.component';
 import { HeaderDataComponent } from './pokemon-db/header-data/header-data.component';
 import {SlideshowModule} from 'ng-simple-slideshow';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -23,6 +29,9 @@ import {SlideshowModule} from 'ng-simple-slideshow';
     AppRoutingModule,
     HttpClientModule,
     SlideshowModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
