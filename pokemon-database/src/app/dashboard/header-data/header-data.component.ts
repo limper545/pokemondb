ef7f8cb44adf2bf5a61addca1d194df860ef69ec
limper545 <limper545@live.com>
2019-06-10 15:16:25 +0200

    Bug fixing and changed style in dashboard

--- pokemon-database/src/app/dashboard/header-data/header-data.component.ts
+++ pokemon-database/src/app/dashboard/header-data/header-data.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
+
 @Component({
   selector: 'pd-header-data',
   templateUrl: './header-data.component.html',
@@ -9,15 +10,11 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HeaderDataComponent implements OnInit {
 
-  // FIXME: Keine Bilder aus dem Internet laden, Observerable mache, damit später wenn ein Pokemon ausgewählt nur Bilder, von diesem erscheinen
   imageSources = [
-    'https://www.lunchactually.com/ch/wp-content/uploads/bfi_thumb/Pokemon-pokemon-13598061-1280-1024-n2boa7sxzkgdc09dp655n80m1wphbv42dt9pnita60.jpg',
-    'https://thehivegaming.s3.us-west-2.amazonaws.com/uploads/2019/01/mCuiXT4KER5lUUbJlCeDS4WcSwgcrKCvHlmRc3vC.jpeg',
-    'https://2ij7hk3p1vra3uhoox411gzf-wpengine.netdna-ssl.com/wp-content/uploads/bfi_thumb/Pokemon-Go-1-o6az9t59e7qemgfdlyztkp1sr63556mo888fqplmm0.jpg'
+    '../../../assets/images/slider_1.jpg',
+    '../../../assets/images/slider_2.jpeg',
+    '../../../assets/images/slider_3.jpg'
   ];
-
-  constructor() {
-  }
   ngOnInit() {
   }
 
